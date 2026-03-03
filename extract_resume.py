34b9389877d1dc577323b85e6ed6849589bd557a
Naveenthiagarajan <149649195+Naveenthiagarajan@users.noreply.github.com>
2026-03-03 16:31:48 +0530

    Delete extract_resume.py

--- extract_resume.py
+++
@@ -1,9 +0,0 @@
-import sys
-import PyPDF2
-pdf_path = r'C:\Users\navee\Naveen\Portfolio\Naveen_Resume.pdf'
-with open(pdf_path, 'rb') as f:
-    reader = PyPDF2.PdfReader(f)
-    text = ''
-    for page in reader.pages:
-        text += page.extract_text() + '\n'
-print(text)
